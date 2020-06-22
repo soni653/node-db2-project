d72ff2546feae5967e4c872d7e25f50307e78b36
soni653 <dave.family653@outlook.com>
2020-06-22 00:06:37 -0400

    Done with stretch

--- cars/cars_router.js
+++ cars/cars_router.js
@@ -42,13 +42,39 @@ router.post("/", async (req, res, next) => {
   }
 });
 
-// router.put("/cars/:id", (req,res,next) => {
-//   try{
-//     const payload = {
-//       name:req.body.name,
+//Put or Update request
 
-//     }
-//   }
-// })
+router.put("/:id", async (req, res, next) => {
+  try {
+    const payload = {
+      VIN: req.body.vin,
+      make: req.body.make,
+      model: req.body.model,
+      mileage: req.body.mileage,
+      transmissionType: req.body.transmissionType,
+      titleStatus: req.body.titleStatus,
+    };
+    await db("cars").update(payload).where("id", req.params.id);
+
+    const car = await db.from("cars").where("id", req.params.id).first();
+
+    res.json(car);
+  } catch (err) {
+    next(err);
+  }
+});
+
+//Delete Request
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    await db("cars").where("id", req.params.id).del();
+
+    const car = await db.first("*").from("cars").where("id", req.params.id);
+    res.json(car);
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
